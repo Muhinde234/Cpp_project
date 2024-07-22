c3a5b5ef4c662809306f1dfd0313e58da5bd8fc7
Igirimpuhwe Dositha <igirimpuhwedosta@gmail>
2024-07-22 20:18:28 +0200

    my very first program in c pp

---
+++ HelloWorldProgram.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+// a very first program for every beginner in c ++
+int main()//main section which holdes the main content of the program
+{
+	//printing  hello world to the screen
+	cout<<"Hello, World!";
+	return 0;
+}
+	
