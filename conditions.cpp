fcee7461cb042aaca3186e0f40377f027d749793
Igirimpuhwe Dositha <igirimpuhwedosta@gmail>
2024-07-22 20:21:49 +0200

    the programs to get familiar with the c pp syntax

---
+++ conditions.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int number;
+cout<<"enter a number: ";
+cin>>number;
+if(number>0)
+{
+	
+cout<<"the number is positive: "<<number;
+}
+else if(number<0)
+{
+cout<<"the number is negative: "<<number;	
+}
+else
+{
+cout<<"number is zero "<<number;
+}	
+}
